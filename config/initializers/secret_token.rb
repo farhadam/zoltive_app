a0c5f429c9ead495e72e678b05f2cbeb492e562f
Farhad <farhad.mibody@gmail.com>
2012-10-21 20:44:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZoltiveApp::Application.config.secret_token = 'da826f3930308d107faec808ec5346cf11b981337e039e6159e3d1ae0b6baacaa76ba92d72616b8b6acc8624be55933f3ca7b85365d068c593017aabc0b75076'
